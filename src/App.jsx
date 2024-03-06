d6ecb66bc6ab48258ffb2c61bbebb6448828aba2
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-03-06 14:25:25 -0300

    fix: MainPage component created to separate from app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import curlingMan from './assets/curling-man.png'
+
 import Header from './components/Header/Header.jsx'
-import arrows from './assets/double-arrow.png'
+import MainPage from './components/MainPage/MainPage.jsx'
 import './App.css'
  
 function App() {
@@ -11,27 +11,7 @@ function App() {
     <>
     <div id='container'>
       <Header />
-      <div id='img-area'>
-        <div id='img-area-text'>
-          Descubra o poder de um 
-          <br />
-          <span className='green'> Corpo saudável</span> <br />
-          
-          <div id='img-area-below-text'> Treine e maximize seu corpo para uma melhor 
-          <br/>
-          <span className='bold-text'>qualidade de vida </span> e aumento da <span className='bold-text'>confiança.</span>
-          </div>
-        </div>
-        <img src={curlingMan} alt="Venha conhecer nossa academia e nossos planos!" id='man' />
-        <div id='img-area-button'>
-          <button id='planos'> Conheça nossos planos </button>
-        </div>      
-          
-      </div>
-      <div>
-        
-      </div>
-  
+      <MainPage></MainPage>
     </div>
     </>
   )
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import curlingMan from '../../assets/curling-man.png'
+import arrows from '../../assets/double-arrow.png'
+import './MainPage.css'
+
+export default function MainPage() {
+  return (
+    <>
+        <div id='img-area'>
+          <div id='img-area-text'>
+              Descubra o poder de um
+              <br />
+              <span className='green'> Corpo saudável</span> <br />
+
+              <div id='img-area-below-text'> Treine e maximize seu corpo para uma melhor
+                  <br />
+                  <span className='bold-text'>qualidade de vida </span> e aumento da <span className='bold-text'>confiança.</span>
+              </div>
+          </div>
+          <img src={curlingMan} alt="Venha conhecer nossa academia e nossos planos!" id='man' />
+          <div id='img-area-button'>
+              <button id='planos'> Conheça nossos planos </button>
+          </div>
+        </div>
+        <div id='arrow-container'>
+          <img id="arrows" src={arrows} alt="pointing arrows" />
+        </div>
+    </>
+  )
+}
