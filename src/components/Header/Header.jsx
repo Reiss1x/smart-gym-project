c053c77efde5342239318a6b3e89677ac623ba86
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-03-11 18:22:56 -0300

    fix: header buttons order fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,10 +17,11 @@ export default function Header() {
     <div className='header'>
         <img src={smartGym} alt="Logo" id='logo'></img>
         <div id='header-buttons'>
-          <button className='h-buttons' onClick={() => handleClick("about-container")}> Sobre </button>
           <button className='h-buttons' onClick={() => handleClick("equipment-container")}> Equipamento </button>
           <button className='h-buttons' onClick={() => handleClick("membership-container")}> Planos </button>
           <button className='h-buttons' onClick={() => handleClick("trainers-container")}> Treinadores </button>
+          <button className='h-buttons' onClick={() => handleClick("about-container")}> Sobre </button>
+
           <button className='h-buttons'> Contato </button>
         </div>
     </div>
