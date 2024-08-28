0d2c7120a53f77ddca903af590eddc4211ae3d82
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-08-28 14:44:26 -0300

    fix: minor fix to membership cars

--- src/components/Membership/membership card/Card.jsx
+++ src/components/Membership/membership card/Card.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import './Card.css'
 import checkMark from '../../../assets/checkmark.png'
 
-export default function ({ data }) {
+export default function ({ data }) {  
+
+  const bestCard = data.best ? '0.5vw solid #72987E' : '0.5vw solid #1C2A32'
+
+
   return (
-    <div className='membership-card'>
+    <div className='membership-card' style={{border: bestCard}}>
         <div className='card-header'>
           <span className='green'>smart</span>{data.title} <br />
           R$ {data.price} / mês
@@ -15,7 +19,7 @@ export default function ({ data }) {
               {data.specifications.map((element, index) => (
                     <div className="specifications" key={index}>
                       <img id="checkmark" src={checkMark}></img>
-                      {element} 
+                      <div id="specifications-text"> {element} </div>
                     </div>
                 ))}
           </div>
